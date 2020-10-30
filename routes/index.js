2117fe35a3ac9c2c404a9706feb54739951c0dc7
khatiwit <69786758+khatiwit@users.noreply.github.com>
2020-10-30 16:23:51 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kubernetes' });
+  res.render('index', { title: 'Kubernetes 01' });
 });
 
 module.exports = router;
